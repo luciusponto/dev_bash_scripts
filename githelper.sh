ac4d57ed6a4817c76324925a374402f32ed77a64
Luciano Scilletta <luciusponto@users.noreply.github.com>
2023-08-13 14:56:23 +0100

    Add new options to git helper script

--- githelper.sh
+++ githelper.sh
@@ -2,46 +2,43 @@
 
 status () {
 	git status
-	quit
 }
 
 log () {
 	git log
-	quit
 }
 
 add_all () {
-	git add .
-	git status
-	quit
+	git add . && git status
 }
 
 add_all_and_commit () {
-	git add .
-	commit
-	git status
-	quit
+	git add . && commit &&	git status
+}
+
+add_commit_push () {
+	git add . && commit && push &&	git status
 }
 
 reset_hard () {
 	git reset --hard && git clean -df
-	quit
 }
 
 push () {
 	git push -u origin main
-	quit
 }
 
 pull () {
 	git pull origin main
-	quit
 }
 
 commit () {
 	read -p "Type commit message: " message
+	if [ "$message" == "" ]; then
+		echo "No commit message supplied. Aborting."
+		return 1
+	fi
 	git commit -m "$message"
-	quit
 }
 
 list_options () {
@@ -49,6 +46,7 @@ list_options () {
 	echo "  l) log"
 	echo "  aa) add ."
 	echo "  ac) add . && commit"
+	echo "  acp) add . && commit && push"
 	echo "  c) commit"
 	echo "  rhc) reset --hard && clean -df (reset hard and delete untracked files)"
 	echo "  push) push -u origin main" 
@@ -62,16 +60,17 @@ quit () {
 do_quit=false
 
 while [ "$do_quit" != "true" ]; do
-	read -p "Choose git command (s/l/aa/ac/c/rhc/push/pull), list options(o) or quit(q): " option
+	read -p "Choose git command (s/l/aa/ac/acp/c/rhc/push/pull), list options(o) or quit(q): " option
 	case $option in
-	  s) status;;
-	  l) log;;
-	  aa) add_all;;
-	  ac) add_all_and_commit;;
-	  c) commit;;
-	  rhc) reset_hard;;
-	  push) push;;
-	  pull) pull;;
+	  s) status; quit;;
+	  l) log; quit;;
+	  aa) add_all; quit;;
+	  ac) add_all_and_commit; quit;;
+	  acp) add_commit_push; quit;;
+	  c) commit; quit;;
+	  rhc) reset_hard; quit;;
+	  push) push; quit;;
+	  pull) pull; quit;;
 	  o) list_options;;
 	  q) quit;;
 	  *) echo "invalid option";;
